4c75a4948adcdaf0148a53ff7b616540ed828990
Nicole Tilbe <nicolecali2406@outlook.com>
2020-09-14 14:24:48 -0400

    finished all tasks

--- src/components/Episodes.js
+++ src/components/Episodes.js
@@ -5,11 +5,11 @@ export default function Episodes(props) {
   return (
     <div className="episodes">
       {props.episodes.map(e => (
-        <div className="episode" key={e.id}>
+        <div className="episode" key={e.id} data-testid='episode'>
           {e.image && (
             <img className="episode-image" src={e.image.medium} alt={e.name} />
           )}
-          <div className="episode-info">
+          <div className="episode-info" >
             <p className="episode-number">
               Season {e.season}, Episode {e.number}
             </p>
